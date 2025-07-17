e46e348ad55825ad2068ec9ec85bc610b2c2b3e6
Wilhan Nunes <85765131+wilhan-nunes@users.noreply.github.com>
2025-07-17 13:27:05 -0700

    updating version control
    Added a function to get the repo latest commit hash.
    This will be used to create an "about" menu with the app version (date) and git hash with link to the original repository for the app.

--- app_homepage.py
+++ app_homepage.py
@@ -1,13 +1,31 @@
 import streamlit as st
 from streamlit.components.v1 import html
+import subprocess
+
+
+def get_git_short_rev():
+    try:
+        return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], stderr=subprocess.DEVNULL).decode().strip()
+    except subprocess.CalledProcessError:
+        return ".git/ not found"
+
 
 # Add a tracking token
 html('<script async defer data-website-id="<your_website_id>" src="https://analytics.gnps2.org/umami.js"></script>', width=0, height=0)
+
+
+# TODO: Bump version
+app_version = "2025-07-17"
+git_hash = get_git_short_rev()
+repo_link = "https://github.com/YOUR-USER/YOUR-REPO"
+
 
 # Write the page label
 st.set_page_config(
     page_title="Homepage",
     page_icon="👋",
+    menu_items={"About": (f"**App Version**: {app_version} | "
+                          f"[**Git Hash**: {git_hash}]({repo_link}/commit/{git_hash})")},
 )
 
 # Getting a text field
